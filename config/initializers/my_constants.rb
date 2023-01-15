78c6ec755154597476cd8fcebfa94bfaae8bab1f
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2023-01-15 16:15:15 +0300

    feat: add room_classes

---
+++ config/initializers/my_constants.rb
@@ -0,0 +1,2 @@
+ROOM_CLASSES = %i[Room-only Standard Minimalist Deluxe Studio Connecting Standard-suite Junior-suites
+                  Presidential-suite Penthouse-suite Honeymoon-suite Bridal-suite].freeze
