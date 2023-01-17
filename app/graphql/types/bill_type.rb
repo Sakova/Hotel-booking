5d9797966cad37f585533cd667be7811e9676e1a
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2023-01-17 16:23:42 +0300

    feat: add bill type

---
+++ app/graphql/types/bill_type.rb
@@ -0,0 +1,8 @@
+module Types
+  class BillType < Types::BaseObject
+    field :id, ID, null: false
+    field :user, UserType, null: false
+    field :request, RequestType, null: false
+    field :room, RoomType, null: false
+  end
+end
