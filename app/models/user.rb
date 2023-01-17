364fde6a47829e3cbe0dd7d2a75ce24340e0e9b7
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2023-01-18 00:07:08 +0300

    feat: add association with bills to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :requests, dependent: :destroy
+  has_many :bills, dependent: :destroy
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
