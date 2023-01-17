bde648012a744b1a7dbc37969ba20d92f2d21baa
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2023-01-18 00:06:42 +0300

    fix: change room_class field to string type

--- app/graphql/types/room_type.rb
+++ app/graphql/types/room_type.rb
@@ -2,7 +2,7 @@ module Types
   class RoomType < Types::BaseObject
     field :id, ID, null: false
     field :places_amount, Integer, null: false
-    field :room_class, Integer, null: false
+    field :room_class, String, null: false
     field :room_number, Integer, null: false
     field :is_free, Boolean, null: false
     field :price, Float, null: false
