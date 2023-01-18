7cb00d3b2f152f37fc93e5e099497a7abd4330c4
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2023-01-18 20:58:47 +0300

    fix: delete empty file

--- config/initializers/my_constants.rb
+++
@@ -1,2 +0,0 @@
-ROOM_CLASSES = %i[Room-only Standard Minimalist Deluxe Studio Connecting Standard-suite Junior-suites
-                  Presidential-suite Penthouse-suite Honeymoon-suite Bridal-suite].freeze
