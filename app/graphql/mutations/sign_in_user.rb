7dc6d9aa3ca832b34e1451685ba39b2e77304176
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2023-01-18 17:09:04 +0300

    fix: raise error when user input invalid credential

--- app/graphql/mutations/sign_in_user.rb
+++ app/graphql/mutations/sign_in_user.rb
@@ -13,7 +13,7 @@ module Mutations
       user = User.find_by email: credentials[:email]
 
       return unless user
-      return unless user.authenticate(credentials[:password])
+      raise 'Invalid email or password' unless user.authenticate(credentials[:password])
 
       token = JsonWebToken.encode(user_id: user.id)
 
