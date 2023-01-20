a164b515d2b1c508bf838a84176869b756cad3c0
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2023-01-20 11:32:51 +0300

    feat: move logic of all requests query to requests_filter

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,8 +2,7 @@ module Types
   class QueryType < Types::BaseObject
     field :users, resolver: Queries::Users
     field :user, resolver: Queries::User
-    field :requests, resolver: Queries::Requests
-    field :requests_filter, resolver: Resolvers::RequestsFilter
+    field :requests, resolver: Resolvers::RequestsFilter
     field :bills, resolver: Resolvers::BillsFilter
     field :rooms, resolver: Queries::Rooms
   end
