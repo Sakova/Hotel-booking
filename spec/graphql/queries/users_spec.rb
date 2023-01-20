4aa4941ceac73a82d7d08bf771745be95cf3db09
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2023-01-20 12:57:00 +0300

    fix: refactor queries tests

--- spec/graphql/queries/users_spec.rb
+++ spec/graphql/queries/users_spec.rb
@@ -4,17 +4,22 @@ RSpec.describe Queries::Users, type: :graphql do
   let(:user) { users(:test) }
   let(:admin) { users(:test_admin) }
 
-  it 'returns array of registered users' do
-    result = HotelBookingSchema.execute(users_query, context: { current_user: admin })
+  let(:variables) { {} }
+  subject(:query_subject) { HotelBookingSchema.execute(users_query, variables: variables, context: ctx) }
 
-    expect(result.dig('data', 'users').length).to eq(users.length)
+  context 'with authenticated admin' do
+    let(:ctx) { { current_user: admin } }
+    it 'returns array of registered users' do
+      expect(subject.dig('data', 'users').length).to eq(users.length)
+    end
   end
 
-  it 'returns an error when current user is not admin' do
-    result = HotelBookingSchema.execute(users_query, context: { current_user: user })
-
-    expect(result.dig('data', 'users')).to be_nil
-    expect(result['errors'].first['message']).to eq('You should be authenticated as admin to perform this action')
+  context 'with authenticated user' do
+    let(:ctx) { { current_user: user } }
+    it 'returns an error' do
+      expect(subject.dig('data', 'users')).to be_nil
+      expect(subject['errors'].first['message']).to eq('You should be authenticated as admin to perform this action')
+    end
   end
 
   def users_query
