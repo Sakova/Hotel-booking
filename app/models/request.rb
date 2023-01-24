2edcbd70554b30f8989b5d5ad75135616bacbfaf
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2023-01-24 10:36:16 +0300

    feat: add comments to models using annotate gem

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: requests
+#
+#  id             :bigint           not null, primary key
+#  comment        :text
+#  places_amount  :integer          not null
+#  room_class     :integer          default("Room-only"), not null
+#  stay_time_from :datetime         not null
+#  stay_time_to   :datetime         not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  user_id        :bigint           not null
+#
+# Indexes
+#
+#  index_requests_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class Request < ApplicationRecord
   belongs_to :user
   has_one :bill, dependent: :destroy
